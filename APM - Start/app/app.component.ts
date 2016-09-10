5fa99999a4ec2b2ca3d0e22c8cfbc42c24c1bd13
Jeremy Gu <isdance2004@hotmail.com>
2016-09-11 07:34:56 +1000

    Chap 05-06

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,8 +1,10 @@
 import {Component} from "angular2/core";
+import {ProductListComponent} from "./product-list.component";
 
 @Component({
     selector : 'pm-app', // pm means product management and app is this class's feature.
-    templateUrl : 'app/templates/app.template.html'
+    templateUrl : 'app/templates/app.template.html',
+    directives: [ProductListComponent]
 })
 export class AppComponent{
     pageTitle : string = "Jeremy Product Management";
